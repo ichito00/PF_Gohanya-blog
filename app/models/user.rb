a7b7c21fb7ab25cc8c6620a92f1d3ed653608940
ichito00 <ichimimo00@gmail.com>
2021-07-16 16:34:40 +0000

    Create Posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,18 +23,17 @@ class User < ApplicationRecord
     followed_relationship.find_by(follower_id: user.id).present?
   end
 
-  def self.search(search,word)
-    if search == "forward_match"
-      @user = User.where("name LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @user = User.where("name LIKE?","%#{word}")
-    elsif search == "perfect_match"
-      @user = User.where(name: word)
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?","%#{word}%")
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
     else
-      @user = User.all
+      User.where('name LIKE ?', '%' + content + '%')
     end
   end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_154734) do
+ActiveRecord::Schema.define(version: 2021_07_16_161713) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "genre_id"
+    t.string "title"
+    t.string "keyword"
+    t.text "body"
+    t.string "image_id"
+    t.boolean "is_deleted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
