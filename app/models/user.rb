6aec23fed0971e143663b343000ca242cc443f1e
ichito00 <ichimimo00@gmail.com>
2021-07-16 16:15:15 +0000

    Create Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,38 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  has_many :follower_relationship, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following_user, through: :follower_relationship, source: :followed
+
+  has_many :followed_relationship, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follower_user, through: :followed_relationship, source: :follower
+
+  attachment :image_id
+
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+
+  def followed_by?(user)
+    followed_relationship.find_by(follower_id: user.id).present?
+  end
+
+  def self.search(search,word)
+    if search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      @user = User.where(name: word)
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
 end
