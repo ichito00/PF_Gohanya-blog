006c718a198c4a561fc675b0dc4d224a316f8ebf
ichito00 <ichimimo00@gmail.com>
2021-07-17 16:37:09 +0000

    [Finish]DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+  if resource_name == :admin
+     admin_root_path
+  elsif resource_name == :user
+     root_path
+  end
+ end
+
+ def after_sign_out_path_for(resource)
+  if resource == :admin
+     new_admin_session_path
+  else
+     new_user_session_path
+  end
+ end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top'
+  
+  
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+  }
 end
